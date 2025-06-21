a425a6b3ef3b809a0fdb622be607ef116946d2b4
estoneman <elstoneman11@gmail.com>
2025-06-21 10:23:56 -0500

    remove www as submodule

---
+++ www/index.js
@@ -0,0 +1,156 @@
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
+import { Cell, Universe } from "wasm-game-of-life";
+
+const CELL_SIZE = 8; // in pixels
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+let universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // vertical lines
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    // horizontal lines
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.moveTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+
+            ctx.fillStyle = cells[idx] === Cell.Dead
+                ? DEAD_COLOR
+                : ALIVE_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+const renderLoop = () => {
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(renderLoop);
+};
+
+};
+
+let animationId = null;
+
+const isPaused = () => {
+    return animationId === null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
+
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+    universe.toggle_cell(row, col);
+
+    drawGrid();
+    drawCells();
+});
+
+const rangeSlider = document.getElementById("range-slider");
+rangeSlider.min = 1;
+rangeSlider.max = 50;
+rangeSlider.value = 1;
+
+const resetRand = document.getElementById("reset-rand");
+resetRand.addEventListener("click", event => {
+    universe.reset_rand();
+
+    drawGrid();
+    drawCells();
+});
+
+const resetDead = document.getElementById("reset-dead");
+resetDead.addEventListener("click", event => {
+    universe.reset_dead();
+
+    drawGrid();
+    drawCells();
+});
+
+const renderLoop = () => {
+    drawGrid();
+    drawCells();
+
+    for (let i = 0; i < rangeSlider.value; i++) {
+        universe.tick();
+    }
+
+    animationId = requestAnimationFrame(renderLoop);
+};
+
+drawGrid();
+drawCells();
+pause();
